0c8f80df3a3d156383aae498c6f7bd1d62804c29
Stef <seedsofthesesame@gmail.com>
2017-09-28 16:34:32 -0700

    Spec_Helper complete.

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,8 +1,21 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require_relative '../lib/driver'
+require_relative '../lib/passenger'
+require_relative '../lib/trip'
+
 # Add simplecov
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Require_relative your lib files here!
+
+#Spec helper from previous projects.
+# require 'simplecov'
+# SimpleCov.start
+# require 'minitest/autorun'
+# require 'minitest/reporters'
+# require 'minitest/skip_dsl'
