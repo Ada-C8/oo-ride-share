ae8272c2b8e2a81ce6be71247c09168671e62409
Stef <seedsofthesesame@gmail.com>
2017-09-28 16:39:40 -0700

    Passenger pseudocode/shell complete.

---
+++ lib/passenger.rb
@@ -0,0 +1,32 @@
+require 'csv'
+
+module RideShare
+  class Passenger
+
+    attr_reader
+
+    def initialize passenger_id, passenger_name, passenger_phone_number
+      @passenger_id = passenger_id
+      @passenger_name = passenger_name
+      @passenger_phone_number = passenger_phone_number # Some numbers have extensions
+    end
+
+    def self.all
+      # This will read in CSV and process them as passenger objects.
+      # Figure out how to check phone numbers here due to extensions this may be difficult.
+    end
+
+    def self.find passenger_id
+      # This will utilize self.all to create an array or array of hashes (still deciding) of passenger instances.
+    end
+
+    def trips
+      # This will return the trips that the passenger has taken.
+    end
+
+    def drivers
+      # Use trip method, connect with the driver_id.
+      # This will need to store the driver instances and then return them.
+    end
+  end
+end
